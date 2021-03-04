c998912bfc674ac66fd4e283597e0a64963c984f
ajghelman <78034606+ajghelman@users.noreply.github.com>
2021-03-04 14:39:15 -0500

    typeahead functinoing

---
+++ public/app.js
@@ -0,0 +1,34 @@
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+const zips = [];
+
+fetch(endpoint)
+    .then(blob => blob.json())
+    .then(data => zips.push(...data))
+
+function findMatches(wordToMatch, zips){
+    return zips.filter(loc => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return loc.zip.match(regex)
+    });
+}
+
+function displayMatches(){
+    const matchArray = findMatches(this.value, zips);
+    const html = matchArray.map(loc => {
+        const regex = new RegExp(this.value, 'gi');
+        const zipNum = loc.zip.replace(regex, `<span class="hl">${this.value}</span>`)
+        return `
+            <li>
+                <span class="name">${loc.name}</span>
+                <span class="zip">${zipNum}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
